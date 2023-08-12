4405e6595c06b90bbf43e3eafa326883995293dc
e.akhantev <e.akhantev@s7.ru>
2023-08-13 02:29:21 +0400

    feat: create function `decode`

--- src/index.js
+++ src/index.js
@@ -39,6 +39,28 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const LETTER_PATTERN = /.{10}/g;
+    const SYMBOL_PATTERN = /.{2}/g;
+    const MORSE_TABLE_NUMBER = {
+        '10': '.',
+        '11': '-',
+        '00': '',
+    }
+
+    let letterArray = expr.match(LETTER_PATTERN);
+    let output = '';
+    for (const letter of letterArray) {
+        let currLet = '';
+        if (letter == '**********') {
+            output = output + ' ';
+        } else {
+            let symbolArray = letter.match(SYMBOL_PATTERN);
+            symbolArray.forEach(element => {currLet = currLet + MORSE_TABLE_NUMBER[element]});
+            output = output + MORSE_TABLE[currLet];
+        }
+
+    }
+    return output;
 }
 
 module.exports = {
